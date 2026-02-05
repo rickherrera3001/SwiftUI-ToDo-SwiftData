e929a27fcf4cdfe384b572b36255baf93274856c
Ricardo Ivan Herrera Rocha <rihr.3001@gmail.com>
2026-02-04 23:59:22 -0600

    feat: updated modelContainer to include Category and TodoItem models

--- App/SwiftUIToDoSwiftDataApp.swift
+++ App/SwiftUIToDoSwiftDataApp.swift
@@ -1,23 +1,17 @@
-//
-//  SwiftUIToDoSwiftDataApp.swift
-//  SwiftUIToDoSwiftData
-//
-//  Created by Ricardo Ivan Herrera Rocha on 21/12/25.
-//
-
 import SwiftUI
 import SwiftData
 
 @main
 struct SwiftUIToDoSwiftDataApp: App {
     
-    // MARK: - Scene
+    // MARK: - Body Scene
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-        
-        // MARK: - Model Container Configuration
-        .modelContainer(for: TodoItem.self)
+        // MARK: - Model Container Setup
+        .modelContainer(for: [TodoItem.self, Category.self])
     }
 }
+EOFv
+
